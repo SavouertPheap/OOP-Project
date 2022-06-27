2a8b07fa8343361d7c740526c95f3e762c9a2877
SAVOUERT.PHEAP <SAVOUERT.PHEAP@pnc.lan>
2022-06-27 20:20:03 +0700

    git pull

--- src/Customer.ts
+++ src/Customer.ts
@@ -2,7 +2,6 @@ import { Gender } from "./staff/Gender";
 import { Person } from "./staff/Person";
 
 export class Customer extends Person{
-    [x: string]: any;
     constructor (name:string,gender:Gender){
         super(name,gender);
     } 
