ea90fddfd5d5181dce629033ee7f5bda8f952e8f
SAVOUERT.PHEAP <SAVOUERT.PHEAP@pnc.lan>
2022-06-30 07:16:30 +0700

    last code

--- dist/Main.js
+++ dist/Main.js
@@ -1,49 +1,30 @@
 "use strict";
-exports.__esModule = true;
-var Table_1 = require("./Materail/Table");
-var Customer_1 = require("./Customer");
-var Gender_1 = require("./staff/Gender");
-var Waiter_1 = require("./staff/Waiter");
-var Resturant_1 = require("./ResturantManagerment/Resturant");
-var Menu_1 = require("./ResturantManagerment/kitchen/Menu");
-var Menuitem_1 = require("./ResturantManagerment/kitchen/Menuitem");
-var Order_1 = require("./OrderManagerment/Order");
-var Chef_1 = require("./staff/Chef");
-// resturant
-var reterants = new Resturant_1.Resturant('Best-Food', 'Phnom penh city');
-// customer that came in resterant
-var sophiem = new Customer_1.Customer('Sophiem', Gender_1.Gender.Female);
-var savouert = new Customer_1.Customer('Savouert', Gender_1.Gender.Female);
-reterants.people.addCustomer(savouert);
-// waiter in resterant 
-var phearun = new Waiter_1.Waiter(sophiem);
-phearun.proviceservice(savouert, sophiem);
-reterants.people.addWaiter(phearun);
-var mengyi = new Waiter_1.Waiter(sophiem);
-mengyi.proviceservice(savouert, sophiem);
-reterants.people.addWaiter(mengyi);
-// table1
-var table1 = new Table_1.Table(1);
-var table2 = new Table_1.Table(2);
-table1.customerTable(savouert);
-table2.customerTable(sophiem);
-console.log(table1.customerTableSit());
-console.log(table2.getTable());
-// menu 
-var menu1 = new Menu_1.Menu(1, 'pizza', 20);
-var menu2 = new Menu_1.Menu(2, 'beaf', 35);
-var menu3 = new Menu_1.Menu(3, 'crap', 30);
-var menu4 = new Menu_1.Menu(4, 'frag', 10);
-var menuItem = new Menuitem_1.MenuItem();
-menuItem.addFood(menu1, menu2, menu3, menu4);
-reterants.addMeunItem(menuItem);
-// order
-var order1 = new Order_1.Order();
-order1.addTable(table1);
-order1.addMenuItem(menuItem);
-// chef
-var meta = new Chef_1.Chef('meta');
-var sophal = new Chef_1.Chef('phal');
-meta.addOrder(order1);
-reterants.people.addChef(meta);
-console.log(reterants);
+Object.defineProperty(exports, "__esModule", { value: true });
+const Customer_1 = require("./Customer");
+const Menu_1 = require("./Menu");
+const MenuItem_1 = require("./MenuItem");
+const Order_1 = require("./Order");
+const Table_1 = require("./Table");
+const Waiter_1 = require("./Waiter");
+let customer1 = new Customer_1.Customer(1, 'savouert');
+let customer2 = new Customer_1.Customer(2, "sopheim");
+// console.log(customer1.getCustomerId());
+let table1 = new Table_1.Table(1);
+let table2 = new Table_1.Table(2);
+let order1 = new Order_1.Order();
+//  order1.orderFood(['Pizz',"ddd"]);
+let waiter1 = new Waiter_1.Waiter();
+waiter1.CreateOrder(1, 1, order1);
+order1.addCustomer(customer1);
+let menu1 = new MenuItem_1.MenuItem(1, 'ddd', 15);
+let menu2 = new MenuItem_1.MenuItem(2, 'dd', 15);
+order1.addMenuItem(menu1, menu2);
+// order1.isMenuEqual(order1)
+let orders = new Menu_1.Menu(['ddd', 'dd1']);
+order1.addFoodCusOrder(orders);
+// console.log(order1);
+// console.log(order1.checkInMenu(orders));
+// checkInMenu
+// console.log(order1.isEquals(orders,menu1));
+console.log(order1.getPriceMenuItem(menu1));
+// console.log(waiter1.isEqual(1));/
---
+++ dist/Status.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Staute = void 0;
+var Staute;
+(function (Staute) {
+    Staute[Staute["Done"] = 0] = "Done";
+    Staute[Staute["Inprogress"] = 1] = "Inprogress";
+})(Staute = exports.Staute || (exports.Staute = {}));
